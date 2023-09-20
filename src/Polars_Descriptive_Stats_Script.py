954dc6f05c6c2e8c4fe78248fb1e30b8df366a2f
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-20 02:53:36 +0000

    ammends

--- src/Polars_Descriptive_Stats_Script.py
+++ src/Polars_Descriptive_Stats_Script.py
@@ -11,14 +11,12 @@
 import lib
 
 # Import the necessary libraries
-def AircraftAnalytics(file_id = '1TAD7Uyc9PjByt_q13uvGXGeubXnujnUi', TARGET_COLUMN = "Aircraft Mass"):
+def AircraftAnalytics(file_id = '1TAD7Uyc9PjByt_q13uvGXGeubXnujnUi',
+                       TARGET_COLUMN = "Aircraft Mass"):
     """This is a function to run some analytics
     on aircraft wildlife accidents data"""   
     # Read our data from Google Drive
-    url = f"https://drive.google.com/uc?id={file_id}"
-
     # Download the contents of the CSV file
-    
     df = lib.read_aircraft_data_from_google_drive(file_id)
     s, m = lib.calculate_strikes_max_damage(df)
     print(lib.return_25th_quantile(df, TARGET_COLUMN))
